22271293b7d5e873f74827d56c2157cfaee7d77a
Bartek <zawadzkibartek@gmail.com>
2018-12-12 19:39:49 +0100

    Project structure + basic examples

---
+++ readme.md
@@ -0,0 +1,13 @@
+
+# Run DB
+
+[Docker HUB for oracle db](https://hub.docker.com/r/wnameless/oracle-xe-11g/)
+
+`docker run -d -p 49161:1521 wnameless/oracle-xe-11g`
+
+    username: system
+    password: oracle
+    
+JDBC url: `jdbc:oracle:thin:@localhost:49161:xe`
+
+
---
+++ src/main/java/Employee.java
@@ -0,0 +1,53 @@
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
+
+
+public class Employee {
+
+  @ColumnName("EMPLOYEE_ID") private int id;
+  @ColumnName("FIRST_NAME") private String firstName;
+  @ColumnName("LAST_NAME") private String lastName;
+  @ColumnName("EMAIL") private String email;
+
+  public Employee(int id, String firstName, String lastName, String email) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public Employee setId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public Employee setFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public Employee setLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public Employee setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+}
